7d0dee28cfebadbcbfcce289e408a75acd1a0e54
washer <madspelt@gmail.com>
2015-12-09 21:07:55 +0100

    tweaked parsing of filename and added comments

--- mp3tagfixer.py
+++ mp3tagfixer.py
@@ -1,4 +1,9 @@
 # Tool to create id3 tags for all mp3 files in folder from filename data.
+# As of now fails when proper song title contains underscores and numbers.
+
+# TODO:
+# Check for multiple word names of artist and album
+# Make genre-tagging work
 
 from mutagen.easyid3 import EasyID3
 from mutagen.mp3 import MP3
@@ -9,8 +14,10 @@ import re
 songfiles = []
 albumname_matches = 0
 artistname_matches = 0
-tag_year = False;
-tag_genre = False;
+album_words = 0
+artist_words = 0
+tag_year = False
+tag_genre = False
 
 #print EasyID3.valid_keys.keys() # prints all valid tags that can be edited with EasyID3
 
@@ -18,6 +25,7 @@ if not len(sys.argv) == 2:
 	sys.exit("You must execute the script with 1 command line argument, containing the path to the folder containing mp3 files.\n"
 		+ "Example: python mp3tagfixer.py /home/<username>/music/<artistname>/<albumname>")
 
+# User input:
 
 print 'Welcome to mp3 tag fixer.\n'
 file_path = sys.argv[1]
@@ -26,13 +34,23 @@ album = raw_input('name of album: ')
 year = raw_input('year of release (optional): ')
 genre = raw_input('genre of album (optional): ')
 
+# Exit if user did not provide artist or album names
+
 if artist is "" or album is "":
 	sys.exit("You must provide artist and album name. Script ending.")
 
+# count words in album and artist name
+
+album_words = len(re.findall(r'\w+', album))
+artist_words = len(re.findall(r'\w+', artist))
+
+# Check if user inputtet genre and year
 if year is not "":
 	tag_year = True
 if genre is not "":
 	tag_genre = True
+
+# Find all mp3 files in dir and sort them by filename.
 
 for root, dirs, files in os.walk(file_path):
 	for file in files:
@@ -56,10 +74,10 @@ for song in songfiles:
 	word_count = 0;
 
 	# song_parts = re.split("\W+|_", song) # splits http://stackoverflow.com/questions/1059559/python-split-strings-with-multiple-delimiters
-	song_parts = song.replace('_',' ').split()
+	song_parts = song.replace('_',' ').split() # split strings with underscore and space as delimeter
 	for part in song_parts:
 		number_ok = True
-		if part.startswith(tuple('0123456789')): 
+		if part.startswith(tuple('0123456789')): # determine if part of string is track number of something else to be discarded
 			for x in part:
 				if not x.isdigit():
 					number_ok = False
@@ -69,25 +87,34 @@ for song in songfiles:
 				else:
 					# SET TRACK NO TO part
 					audio["tracknumber"] = part
-		else:
+		else: # parse part of string that is not a number and decide what to do with it
 			word = ""
 			word_count += 1
-			if 'mp3' in part:
+			if 'mp3' in part: # check if part contains suffix, if so remove it.
 				idx = part.find('.mp3')
 				word = part[:idx]
 			else:
-				word = part
-			if albumname_matches > 1:
-				if part.upper().lower() == album.upper().lower():
+				word = part 
+			if albumname_matches > 1: # check if word is artist or album name in filename. If so, discard.
+				if word.upper().lower() == album.upper().lower():
 					continue
+				else if album_words > 1:
+					if word.upper().lower() in album:
+						continue
+
 			if artistname_matches > 1:
-				if part.upper().lower() == artist.upper().lower():
+				if word.upper().lower() == artist.upper().lower():
 					continue
+				else if artist_words > 1:
+					if word.upper().lower() in artist:
+						continue
+			# append word to title title string if appropriate
 			if word_count > 1:
 				title_str = title_str + " " + word.title()
 			else:
 				title_str = title_str + word.title()
 
+	# add tags to file and save
 	audio["artist"] = u"" + artist
 	audio["album"] = u"" + album
 	audio["title"] = u"" + title_str
